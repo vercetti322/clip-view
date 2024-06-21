64351a6aef3632320585fca8156d17f61bcd3988
vercetti322 <vercettitommy322@gmail.com>
2024-06-21 22:16:58 +0530

    basic text widget made!

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import font
 
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
-
+# Initialize the Tkinter application
 root = Tk()
-root.title("Feet to Meters")
+root.title('View Clipboard')
+root.geometry('600x150')
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+# Create the mainframe with padding
+mainframe = ttk.Frame(root, padding='15')
+mainframe.pack(expand=True, fill='both')  # Pack mainframe to fill root window
 
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=22, textvariable=feet)
-feet_entry.grid(column=1, row=1, sticky=(W, E))
+# make custom font
+custom_font = font.Font(family='Roboto', size=14)
 
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+# Create the Text widget for clipboard content
+text_widget = Text(mainframe, padx=5, pady=5, font=custom_font, wrap='word')
+text_widget.pack(expand=True, fill='both')  # Pack text widget to fill mainframe
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+# make Scroll Bar
+scrollbar = ttk.Scrollbar(mainframe, orient='vertical', command=text_widget.yview)
+scrollbar.pack(side='right', fill='y')
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+# config the text widget for this
+text_widget.config(yscrollcommand=scrollbar.set)
 
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
+# Insert dummy text into the Text widget
+text_widget.insert('1.0', 'Our independent, member-supported model means we don’t sell your attention (or data) to advertisers. Our focus is on connecting you to stories that matter. \
+Our independent, member-supported model means we don’t sell your attention (or data) to advertisers. Our focus is on connecting you to stories that matter. \
+Our independent, member-supported model means we don’t sell your attention (or data) to advertisers. Our focus is on connecting you to stories that matter.')
 
-feet_entry.focus()
-root.bind("<Return>", calculate)
+# make the text read-only
+text_widget.config(state='disabled')
 
+# Start the Tkinter event loop
 root.mainloop()
